97655eb1c243b1a5391c15402537fc30e6b74355
Maxim <bboyxaker777@gmail.com>
2020-03-06 17:51:01 +0600

    Механизм прикрепления скан файла в админке академ отпуска и отправка по емейлу

--- ssc/admin.py
+++ ssc/admin.py
@@ -3,7 +3,6 @@ from ssc.models import *
 from ssc.utilities import *
 from django.contrib import admin
 
-
 # Заголовки админ.сайта
 admin.site.index_title = 'Центр обслуживания студентов'
 admin.site.site_header = 'Центр обслуживания студентов'
@@ -173,6 +172,7 @@ class AcademicLeaveAdmin(CustomAdmin):
     Админ.панель академ.отпусков
     """
     entity = 'academic-leave'
+    change_form_template = "custom_admin/academic-leave.html"
     mail_template = 'mails/academic-leave.html'
     app = 'Ваш приказ готов. Вы можете получить его в КарГТУ, 1 корпус, кабинет № 109.'
     list_per_page = 15
@@ -191,6 +191,63 @@ class AcademicLeaveAdmin(CustomAdmin):
     def id_card_back(self, obj):
         return format_html(f"""<img src="{obj.iin_attachment_back.url}" width="300px">""")
 
+    def response_change(self, request, obj):
+        # Если заявление заполнено неправильно, отправляем письмо с уведомлением
+        if "_send_for_correction" in request.POST:
+            if obj.status != 'Отозвано на исправление':
+                note = request.POST.get('note')
+
+                obj.status = 'Отозвано на исправление'
+                obj.save()
+
+                ctx = {'name': obj.first_name,
+                       'note': note}
+                to = (obj.email,)
+                send_email('mails/revoke.html', ctx, to)
+                self.message_user(request, f"Письмо с уведомлением отправлено {obj}")
+            else:
+                self.message_user(request, f"Письмо с уведомлением уже отправлено {obj}")
+
+        # Потверждение заявления
+        if "_verify" in request.POST:
+            # Если подтвержден - выдаем сообщение, что заявление уже подтверждено
+            if obj.status == 'Подтверждено':
+                self.message_user(request, f"{obj} уже потвержден")
+            # Если не потверждено - подтверждаем и отправляем письмо на почту
+            else:
+                obj.status = 'Подтверждено'
+                obj.save()
+
+                # отправляем письмо после потверждения заявления
+                ctx = {'name': request.POST['first_name']}
+                to = (request.POST.get('email', ''),)
+
+                send_email('mails/ready.html', ctx, to)
+
+                self.message_user(request, f"""{obj} подтверждено""")
+
+        # Завершение обработки заявления
+        if "_finish" in request.POST:
+            # Если завершено - выдаем сообщение, что заявление уже завершено
+            if obj.status is 'Завершено':
+                self.message_user(request, f"{obj} обработка завершена")
+            # Если не завершено - завершаем и отправляем письмо на почту
+            else:
+                obj.status = 'Завершено'
+                obj.save()
+
+                ctx = {'name': obj.first_name,
+                       'app': self.app}
+                to = (obj.email,)
+
+                uploaded_file = request.FILES['scanned_file']
+
+                send_email_with_attachment("mails/ready.html", ctx, to, uploaded_file)
+
+                self.message_user(request, f"""Обработка заявления "{obj}" завершена. Письмо отправлено""")
+
+        return super().response_change(request, obj)
+
 
 @admin.register(Abroad)
 class AbroadAdmin(CustomAdmin):
@@ -208,7 +265,7 @@ class AbroadAdmin(CustomAdmin):
     search_fields = ('last_name', 'first_name', 'patronymic', 'address',
                      'individual_identification_number')
 
-    autocomplete_fields = ('university', )
+    autocomplete_fields = ('university',)
 
     readonly_fields = ('id_card_front', 'id_card_back')
 
@@ -219,35 +276,57 @@ class AbroadAdmin(CustomAdmin):
         return format_html(f"""<img src="{obj.iin_attachment_back.url}" width="300px">""")
 
     def response_change(self, request, obj):
-        # Потверждение заявления
-        if "_verify" in request.POST:
-            # Если потвержден - выдаем сообщение, что заявление уже потверждено
-            if obj.status == 'Завершено':
-                self.message_user(request, f"{obj} уже потвержден")
-            # Если не потверждено - потверждаем и отправляем письмо на почту
-            else:
-                obj.status = 'Завершено'
-                obj.save()
-
-                # Сотрудники международного отдела, пишут письма студентам сами
-                # Убираем автоматическое уведомление электронной почтой
-
-                self.message_user(request, f"""Обработка заявления "{obj}" завершена""")
-            # return HttpResponseRedirect(".")
         # Если заявление заполнено неправильно, отправляем письмо с уведомлением
         if "_send_for_correction" in request.POST:
-            if obj.status is not 'Отозвано на исправление':
+            if obj.status != 'Отозвано на исправление':
                 note = request.POST.get('note')
-                to = (obj.email,)
-                ctx = {'name': obj.first_name,
-                       'note': note}
+
                 obj.status = 'Отозвано на исправление'
                 obj.save()
 
+                ctx = {'name': obj.first_name,
+                       'note': note}
+                to = (obj.email,)
                 send_email('mails/revoke.html', ctx, to)
                 self.message_user(request, f"Письмо с уведомлением отправлено {obj}")
             else:
                 self.message_user(request, f"Письмо с уведомлением уже отправлено {obj}")
+
+        # Потверждение заявления
+        if "_verify" in request.POST:
+            # Если подтвержден - выдаем сообщение, что заявление уже подтверждено
+            if obj.status == 'Подтверждено':
+                self.message_user(request, f"{obj} уже потвержден")
+            # Если не потверждено - подтверждаем и отправляем письмо на почту
+            else:
+                obj.status = 'Подтверждено'
+                obj.save()
+
+                # отправляем письмо после потверждения заявления
+                ctx = {'name': request.POST['first_name']}
+                to = (request.POST.get('email', ''),)
+
+                send_email(self.mail_template, ctx, to)
+
+                self.message_user(request, f"""{obj} подтверждено""")
+
+        # Завершение обработки заявления
+        if "_finish" in request.POST:
+            # Если завершено - выдаем сообщение, что заявление уже завершено
+            if obj.status is 'Завершено':
+                self.message_user(request, f"{obj} обработка завершена")
+            # Если не завершено - завершаем и отправляем письмо на почту
+            else:
+                obj.status = 'Завершено'
+                obj.save()
+
+                ctx = {'name': obj.first_name,
+                       'app': self.app}
+                to = (obj.email,)
+                send_email('mails/ready.html', ctx, to)
+
+                self.message_user(request, f"""Обработка заявления "{obj}" завершена. Письмо отправлено""")
+
         return super().response_change(request, obj)
 
 
